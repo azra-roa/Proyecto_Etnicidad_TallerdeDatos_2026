74d5a3ad41145f59b72e4a75b145dd4f892bd059
azra.roa <azra.roa@pucp.edu.pe>
2026-06-19 21:08:34 -0500

    Se crearon las carpetas para el Proyecto Etnicidad

---
+++ Creación de carpetas.R
@@ -0,0 +1,15 @@
+#===========================================================================================
+#Proyecto: Análisis de la relacion etnicidad-salario usando datos de la ENAHO
+#Autorx: Azra Roa
+#Objetivo de este script: Crear el sistema de carpetas 
+#Fecha: 17-06-2026
+#==========================================================================================
+
+#Creamos carpetas--------------------------
+
+dir.create("datos")
+dir.create("datos/crudos")
+dir.create("datos/procesados")
+dir.create("outputs")
+dir.create("docs")
+dir.create("scripts") 
