b08ac98b3c9c74242868bacffff3daf520fb7a5a
azra.roa <azra.roa@pucp.edu.pe>
2026-06-19 21:48:24 -0500

    Creamos script para carga y unión de los modulos de la ENAHO necesarios para el Proyecto Etnicidad

---
+++ scripts/01_Carga_Unión_Modulos.R
@@ -0,0 +1,14 @@
+#===========================================================================================
+#Proyecto: Análisis de la relacion etnicidad-salario usando datos de la ENAHO
+#Autorx: Azra Roa
+#Objetivo de este script: Cargar modulos y hacer joins
+#Fecha: 17-06-2026
+#==========================================================================================
+
+#1. Carga de librerias------------------------
+library(rio)
+library(janitor)
+library(haven) 
+library(dplyr) 
+library(tidyr)
+
